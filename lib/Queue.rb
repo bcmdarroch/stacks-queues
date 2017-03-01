9a3fa7682942a088d702fcd2b0c283ec508519c1
Brenna <bcmdarroch@gmail.com>
2017-03-01 10:54:50 -0800

    implemented Stack and Queue

--- lib/Queue.rb
+++ lib/Queue.rb
@@ -1,22 +1,33 @@
 class Queue
   def initialize
+    @store = Array.new
   end
 
+# beginning of array is front of queue
   def enqueue(element)
+    @store << element
   end
 
   def dequeue
+    raise ArgumentError.new("You can't dequeue an empty queue!") if empty?
+    # shift removes first item of array
+    @store.shift
   end
 
   def front
+    raise ArgumentError.new("You can't call front on an empty queue!") if empty?
+    @store[0]
   end
 
   def size
+    @store.length
   end
 
   def empty?
+    size == 0
   end
 
   def to_s
+    @store.to_s
   end
 end
--- lib/Stack.rb
+++ lib/Stack.rb
@@ -3,19 +3,27 @@ class Stack
     @store = Array.new
   end
 
+# end of array is considered 'top' of stack
   def push(element)
+    return @store << element
   end
 
   def pop
+    raise ArgumentError.new("You can't pop from an empty array!") if empty?
+    return @store.pop
   end
 
   def top
+    raise ArgumentError.new("You can't call top on an empty array!") if empty?
+    return @store[-1]
   end
 
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0
   end
 
   def to_s
